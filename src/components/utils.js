0e5706116f2c1739984c3cb839e001cc3978cc21
brandodo <brandonong93@gmail.com>
2022-05-10 02:18:54 -0400

    fixed heroku link

--- src/components/utils.js
+++ src/components/utils.js
@@ -1,6 +1,6 @@
 const API_URL =
   process.env.NODE_ENV === "production"
-    ? "https://flexooffice-server.heroku.com"
+    ? "https://flexooffice-server.herokuapp.com"
     : "http://localhost:8080";
 
 export default API_URL;
